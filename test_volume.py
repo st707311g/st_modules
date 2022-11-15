f7fde8aabea711d245be209b1caf565df6a0ed26
st707311g <st707311g@gmail.com>
2022-11-15 15:50:19 +0900

    updated to version 1.1 (bug fix)

--- test_volume.py
+++ test_volume.py
@@ -59,14 +59,88 @@ class TestVolume3D(unittest.TestCase):
             )
 
 
-class TestVolume(unittest.TestCase):
+class TestVolumeLoader(unittest.TestCase):
     def setUp(self):
+        # // making test volume
         self.test_volume3d_instance = Volume3D(
             np_volume=np.random.randint(
                 0, 255, size=(128, 64, 32), dtype=np.uint8
             ),
             mm_resolution=0.3,
         )
+
+        # // making temporary directory
+        self.temporary_directory_p = TemporaryDirectory()
+        self.temporary_directory_path = Path(self.temporary_directory_p.name)
+
+        volume_saver = VolumeSaver(self.test_volume3d_instance)
+        for i, total in volume_saver.save_files_iterably(
+            destination_directory=self.temporary_directory_path,
+            extension="png",
+        ):
+            pass
+
+        # // making temporary archive
+        self.temporary_archive_p = NamedTemporaryFile(suffix=".tar.gz")
+        self.temporary_archive_path = Path(self.temporary_archive_p.name)
+
+        for i, total in volume_saver.save_volume_as_archive_iterably(
+            archive_path=self.temporary_archive_path, extension="png"
+        ):
+            pass
+
+    def tearDown(self) -> None:
+        self.temporary_directory_p.cleanup()
+        self.temporary_archive_p.close()
+
+    def test_volume_load(self):
+        with self.assertRaises(AssertionError):
+            volume_loader = VolumeLoader(
+                volume_path=Path(str(self.temporary_directory_path) + "_")
+            )
+
+        # // test loading
+        for target in [
+            self.temporary_directory_path,
+            str(self.temporary_directory_path),
+            self.temporary_archive_path,
+            str(self.temporary_archive_path),
+        ]:
+            volume_loader = VolumeLoader(volume_path=target)
+            self.assertTrue(volume_loader.is_valid_volume())
+
+            self.assertEqual(
+                volume_loader.image_file_number,
+                self.test_volume3d_instance.shape[0],
+            )
+
+            for i, total in volume_loader.load_files_iterably():
+                pass
+            self.assertEqual(i + 1, total)
+
+            volume3d = volume_loader.get()
+            self.assertEqual(
+                volume3d.mm_resolution,
+                self.test_volume3d_instance.mm_resolution,
+            )
+            self.assertTrue(
+                (
+                    self.test_volume3d_instance.np_volume == volume3d.np_volume
+                ).all()
+            )
+
+
+class TestVolumeSaver(unittest.TestCase):
+    def setUp(self):
+        # // making test volume
+        self.test_volume3d_instance = Volume3D(
+            np_volume=np.random.randint(
+                0, 255, size=(128, 64, 32), dtype=np.uint8
+            ),
+            mm_resolution=0.3,
+        )
+
+        # // making temporary directory
         self.temporary_directory_p = TemporaryDirectory()
         self.temporary_directory_path = self.temporary_directory_p.name
 
@@ -132,69 +206,23 @@ class TestVolume(unittest.TestCase):
 
             self.assertTrue(os.path.isfile(temporary_file.name))
 
-    def test_load_archive_volume(self):
-        self.test_volume3d_instance.mm_resolution = 0.6
-        volume_saver = VolumeSaver(self.test_volume3d_instance)
 
-        with NamedTemporaryFile(suffix=".tar.gz") as temporary_file:
-            for i, total in volume_saver.save_volume_as_archive_iterably(
-                archive_path=temporary_file.name, extension="png"
-            ):
-                pass
+class TestVolumeSeparator(unittest.TestCase):
+    def setUp(self):
+        # // making test volume
+        self.test_volume3d_instance = Volume3D(
+            np_volume=np.random.randint(
+                0, 255, size=(128, 64, 32), dtype=np.uint8
+            ),
+            mm_resolution=0.3,
+        )
 
-            volume_loader = VolumeLoader(temporary_file.name)
-            for i, total in volume_loader.load_files_iterably():
-                pass
-            self.assertEqual(i + 1, total)
+        # // making temporary directory
+        self.temporary_directory_p = TemporaryDirectory()
+        self.temporary_directory_path = self.temporary_directory_p.name
 
-            volume3d = volume_loader.get()
-
-            self.assertEqual(
-                volume3d.mm_resolution,
-                self.test_volume3d_instance.mm_resolution,
-            )
-            self.assertTrue(
-                (
-                    self.test_volume3d_instance.np_volume == volume3d.np_volume
-                ).all()
-            )
-
-    def test_volume_load(self):
-        volume_saver = VolumeSaver(self.test_volume3d_instance)
-
-        with TemporaryDirectory() as temporary_directory:
-            for i, total in volume_saver.save_files_iterably(
-                destination_directory=temporary_directory, extension="png"
-            ):
-                pass
-
-            with self.assertRaises(AssertionError):
-                volume_loader = VolumeLoader(
-                    volume_path=temporary_directory + "_"
-                )
-
-            volume_loader = VolumeLoader(volume_path=temporary_directory)
-            self.assertTrue(volume_loader.is_valid_volume())
-
-            self.assertEqual(
-                volume_loader.image_file_number,
-                self.test_volume3d_instance.shape[0],
-            )
-
-            for i, total in volume_loader.load_files_iterably():
-                pass
-            self.assertEqual(i + 1, total)
-
-            volume3d = volume_loader.get()
-            self.assertEqual(
-                volume3d.mm_resolution,
-                self.test_volume3d_instance.mm_resolution,
-            )
-            self.assertTrue(
-                (
-                    self.test_volume3d_instance.np_volume == volume3d.np_volume
-                ).all()
-            )
+    def tearDown(self) -> None:
+        self.temporary_directory_p.cleanup()
 
     def test_volume_separator(self):
         test_block_size, test_overlap = (64, 64, 64), 8
